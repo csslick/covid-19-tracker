840ef19b61755acfeaff40294779ce596134b05b
04-Main <digitone@naver.com>
2021-08-06 11:21:36 +0900

    api 경로 수정

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# covid-19-tracker
+# covid-19-tracker(코로나 실시간 상황판)
 covid-19 Tracker  
+
+1. 일별 확진자 정보
+2. 국가별 조회
+3. 추이 그래프(미구현)
+
 demo: https://csslick.github.io/covid-19-tracker/
 
 참조  
--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 // API URL https://documenter.getpostman.com/view/10808728/SzS8rjbc#63fda84a-6b43-4506-9cc7-2172561d5c16
 
-// 국가별 코로나 실시간 현황 조회
-const API_URL = `https://api.covid19api.com/live/country/`;
-const country = {
-  kr: 'south-korea',
-  us: 'usa',
-  jp: 'japan',
-  ci: 'china',
-}
+// 국가별 코로나 일별 누적 조회
+const API_URL = `https://api.covid19api.com/total/dayone/country/`;
 
 function getCovidData(country) {
   fetch(`${API_URL}${country}`)
@@ -21,12 +15,20 @@ function getCovidData(country) {
 }  
 
 function showCovidData(data) {
-  // 코로나 데이터 속성
+  // 국가(지역)
   const country = data[data.length-1].Country;
+  // 격리자
   const active = data[data.length-1].Active;
+  const activeDay = data[data.length-1].Active - data[data.length-2].Active;;
+  // 사망자
   const deaths = data[data.length-1].Deaths;
+  const deathsDay = data[data.length-1].Deaths - data[data.length-2].Deaths;
+  // 격리해제
   const recovered = data[data.length-1].Recovered;
+  const recoveredDay = data[data.length-1].Recovered - data[data.length-2].Recovered;;
+  // 확진자
   const confirmed = data[data.length-1].Confirmed;
+  const confirmedDay = data[data.length-1].Confirmed - data[data.length-2].Confirmed;
   const date = data[data.length-1].Date.slice(0, 10);
   console.log(country, active, deaths, recovered, confirmed, date);
 
@@ -40,10 +42,10 @@ function showCovidData(data) {
 
   // DOM 엘리먼트에 데이터 추가
   countryEl.innerHTML = country;
-  activeEl.innerHTML = active.toLocaleString();
-  deathsEl.innerHTML = deaths.toLocaleString();
-  recoveredEl.innerHTML = recovered.toLocaleString();
-  confirmedEl.innerHTML = confirmed.toLocaleString();
+  activeEl.innerHTML = activeDay.toLocaleString();
+  deathsEl.innerHTML = deathsDay.toLocaleString();
+  recoveredEl.innerHTML = recoveredDay.toLocaleString();
+  confirmedEl.innerHTML = confirmedDay.toLocaleString();
   dateEl.innerHTML = date;
 }  
 
@@ -56,5 +58,5 @@ dropdown.addEventListener('change', function(){
 });
 
 
-getCovidData(country.kr)
+getCovidData('south-korea')
 
